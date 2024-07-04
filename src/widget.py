705298ebcf2adbbcf4ebce7aa9c8d3b9b5ce2d2a
Alexandr Pavlushenko <a.s.pavlushenko@gmail.com>
2024-07-04 14:38:26 +0300

    edit README.md

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,14 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(acc_card_number: str) -> str | None:
     """Функция общей маскировки карты и счета"""
-    acc_card_list = acc_card_number.split(' ')
-    if len(acc_card_list) == 2 and acc_card_list[0] == 'Счет':
+    acc_card_list = acc_card_number.split(" ")
+    if len(acc_card_list) == 2 and acc_card_list[0] == "Счет":
         return f"Счет {get_mask_account(acc_card_list[-1])}"
-    elif len(acc_card_list) == 3 and acc_card_list[0] == 'Visa':
+    elif len(acc_card_list) == 3 and acc_card_list[0] == "Visa":
         return f"Visa {acc_card_list[1]} {get_mask_card_number(acc_card_list[-1])}"
-    elif len(acc_card_list) == 2 and acc_card_list[0] == 'Maestro' or acc_card_list[0] == 'MasterCard':
+    elif len(acc_card_list) == 2 and acc_card_list[0] == "Maestro" or acc_card_list[0] == "MasterCard":
         return f"{acc_card_list[0]} {get_mask_card_number(acc_card_list[-1])}"
     else:
         return None
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,32 +1,25 @@
-from src.masks import get_mask_card_number, get_mask_account
-
-
 import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == '7000 79** **** 6361'
+    assert get_mask_card_number(card_number) == "7000 79** **** 6361"
 
 
-@pytest.mark.parametrize('value, expected', [
-    ('7000', None),
-    ('70007922896063611', None),
-    ('', None),
-    ('abcd792289606361', None)
-])
+@pytest.mark.parametrize(
+    "value, expected", [("7000", None), ("70007922896063611", None), ("", None), ("abcd792289606361", None)]
+)
 def test_get_mask_card_numbers(value, expected):
     assert get_mask_card_number(value) == expected
 
 
 def test_get_mask_account(acc_number):
-    assert get_mask_account(acc_number) == '**4305'
+    assert get_mask_account(acc_number) == "**4305"
 
 
-@pytest.mark.parametrize('value, expected', [
-    ('7365', None),
-    ('736541084301358743055', None),
-    ('', None),
-    ('7365410843013587abcd', None)
-])
+@pytest.mark.parametrize(
+    "value, expected", [("7365", None), ("736541084301358743055", None), ("", None), ("7365410843013587abcd", None)]
+)
 def test_get_mask_accounts(value, expected):
     assert get_mask_account(value) == expected
