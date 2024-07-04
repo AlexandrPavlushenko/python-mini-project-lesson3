ea7968ccc6e973eec8bbd5587d587213c6ffd4a2
Alexandr Pavlushenko <a.s.pavlushenko@gmail.com>
2024-07-04 13:53:45 +0300

    test_processing.py finish

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,2 +1,27 @@
 from src.processing import filter_by_state, sort_by_date
 
+
+def test_filter_by_state(state_list):
+    assert filter_by_state(state_list) == [{'id': 414288295, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+def test_filter_by_state_canceled(state_list):
+    assert filter_by_state(state_list, state_value='CANCELED') == [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def test_sort_by_date(state_list):
+    assert sort_by_date(state_list) == [{'id': 414288295, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, ]
+
+
+def test_sort_by_date_reverse(state_list):
+    assert sort_by_date(state_list, reverse_date=False) == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 414288295, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
