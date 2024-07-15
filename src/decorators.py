1b1c95b10545b3d66ed5616c8a430ea283fd98a0
Alexandr Pavlushenko <a.s.pavlushenko@gmail.com>
2024-07-15 17:17:35 +0300

    README.md edit

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,26 @@
+from functools import wraps
+
+
 def log(filename=None):  # filename - имя файла для записи логов
     """Декоратор для логирования вызовов функции."""
 
     def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, "a") as log_file:
-                        log_file.write("my_function ok")
+                    with open(filename, "a", encoding="utf-8") as log_file:
+                        log_file.write(f"{func.__name__} ok\n")
                 else:
-                    print("my_function ok")
+                    print(f"{func.__name__} ok")
                 return result
             except Exception as e:
                 if filename:
-                    with open(filename, "a") as log_file:
-                        log_file.write(f"my_function error: {e}. Inputs: {args}, {kwargs}")
+                    with open(filename, "a", encoding="utf-8") as log_file:
+                        log_file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
                 else:
-                    print(f"my_function error: {e}. Inputs {args}, {kwargs}")
-                raise
+                    print(f"{func.__name__} error: {e}. Inputs {args}, {kwargs}")
 
         return wrapper
 
@@ -26,7 +29,8 @@ def log(filename=None):  # filename - имя файла для записи ло
 
 @log()
 def my_function(x, y):
-    return x / y
+    """Декорируемая функция"""
+    return x + y
 
 
-my_function(1, 2)
+my_function(2, 0)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,16 +1,67 @@
+import tempfile
+
 import pytest
 
-from src.decorators import my_function
+from src.decorators import log, my_function
 
 
-def test_log():
-    with pytest.raises(TypeError):
-        my_function("1", 2)
-    with pytest.raises(ZeroDivisionError):
-        my_function(1, 0)
+def test_log_good(capsys):
+    """Тест выполнения декорированной функции"""
+
+    @log()
+    def func(x, y):
+        return x + y
+
+    result = func(1, 2)
+
+    assert result == 3
 
 
-def test_console_output(capsys):
-    result = my_function(3, 1)
+def test_log_good_file_log(capsys):
+    """Тест записи в файл после успешного выполнения"""
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def func(x, y):
+        return x + y
+
+    func(1, 2)
+
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+
+    assert f"{func.__name__} ok" in logs
+
+
+def test_log_exception(capsys):
+    """Тест вывода после ошибки в консоль"""
+
+    @log()
+    def func(x, y):
+        return x + y
+
+    func(1, "2")
+
     captured = capsys.readouterr()
-    assert captured.out == "my_function ok\n"
+
+    assert f"{func.__name__} error:" in captured.out
+
+
+def test_log_exception_file_log(capsys):
+    """Тест записb в файл после ошибки"""
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def func(x, y):
+        return x + y
+
+    func(1, "2")
+
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+
+    assert f"{func.__name__} error" in logs
