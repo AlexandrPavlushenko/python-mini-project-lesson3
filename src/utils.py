4fc966881d2d0d454d903244fe654c13b3e35083
AlexandrPavlushenko <a.s.pavlushenko@gmail.com>
2024-07-23 20:28:52 +0300

    jsonError

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ def json_to_list_dict(path_to_json_file):
     try:
         with open(path_to_json_file, "r", encoding="utf-8") as file:
             data = json.load(file)
-    except Exception:
+    except json.JSONDecodeError:
+        return []
+    except FileNotFoundError:
         return []
     return data
