34dfee234ff7f68aca51e12fb8913f8c7f9c18c2
AlexandrPavlushenko <a.s.pavlushenko@gmail.com>
2024-07-23 04:32:59 +0300

    external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,23 @@
 import requests
 
+from dotenv import load_dotenv
 
-def amount_transaction_rub(transactions_data: list[dict]) -> float:
-    """Функция расчета суммы транзакций в рублях"""
+import os
+
+
+def conversion_to_rub(currency, amount):
+    """Функция конвертации валюты в рубли"""
+
+    try:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        load_dotenv('../.env')
+        apikey = os.getenv("API_KEY_EXCHANGE_RATES")
+        headers = {"apikey": apikey}
+        response = requests.get(url, headers=headers)
+        result = response.json()
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
+    return round(result.get("result"), 2)
+
 
