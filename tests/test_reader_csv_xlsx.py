349177316dd4f51c3e3c519a1f6cb5e39e4bde99
AlexandrPavlushenko <a.s.pavlushenko@gmail.com>
2024-07-30 00:29:35 +0300

    edit_test_reader_csv_xlsx.py

--- tests/test_reader_csv_xlsx.py
+++ tests/test_reader_csv_xlsx.py
@@ -13,8 +13,8 @@ class TestGetDataCsv(unittest.TestCase):
         "builtins.open",
         new_callable=mock_open,
         read_data="id;state;date;amount;currency_name;currency_code;from;to;"
-        "description\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;"
-        "PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации\n",
+                  "description\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;"
+                  "PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации\n",
     )
     def test_get_data_csv(self, mock_file):
         expected_result = [
@@ -31,7 +31,6 @@ class TestGetDataCsv(unittest.TestCase):
             }
         ]
 
-        # Calling a function with a substituted file
         result = get_data_csv("fake_path.csv")
 
         self.assertEqual(result, expected_result)
@@ -40,7 +39,6 @@ class TestGetDataCsv(unittest.TestCase):
 class TestGetDataXlsx(unittest.TestCase):
     @patch("pandas.read_excel")
     def test_get_data_xlsx(self, mock_read_excel):
-        # Создаем пример DataFrame, который мы ожидаем получить
         mock_data = [
             {
                 "id": "650703",
@@ -59,7 +57,6 @@ class TestGetDataXlsx(unittest.TestCase):
 
         result = get_data_xlsx("fake_file.xlsx")
 
-        # Ожидаемый результат
         expected_result = [
             {
                 "id": "650703",
